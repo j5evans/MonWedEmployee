988bc2df615307354b4012e8a9cf7f373fe32087
j5evans <j5evans@student.bridgew.edu>
2021-10-06 16:03:36 -0400

    initial commit

---
+++ src/comp/comp152/Employee.java
@@ -0,0 +1,14 @@
+package comp.comp152;
+
+public abstract class Employee {
+
+    public void fileW2Forms() {
+        System.out.println("Here are your W2 forms.");
+    }
+
+    public void withholdTaxes() {
+        System.out.println("Removing " + calculateBiWeeklyPay() * 0.1 +  " of your pay for taxes.");
+    }
+
+    public abstract double calculateBiWeeklyPay();
+}
